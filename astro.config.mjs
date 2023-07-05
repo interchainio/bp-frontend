935cfe93adf960ba15f69c8549f9d7ca671603a5
Noam Cohen <81436914+nooomski@users.noreply.github.com>
2023-07-05 18:45:37 +0200

    add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vercelStatic from '@astrojs/vercel/static';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: vercelStatic()
 });
