f098b2e72b7a415c0ca504aa7303b8355252aa84
Noam Cohen <81436914+nooomski@users.noreply.github.com>
2023-07-05 19:01:48 +0200

    Revert "add vercel config"
    This reverts commit 935cfe93adf960ba15f69c8549f9d7ca671603a5.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import vercelStatic from '@astrojs/vercel/static';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  adapter: vercelStatic()
+  integrations: [tailwind()]
 });
